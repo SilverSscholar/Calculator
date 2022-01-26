0007d39503506d73ed9f933c60e25f49224eee05
Steve Jones <jones.wiredbraincoffee@gmail.com>
2022-01-26 13:31:33 -0500

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.One = new System.Windows.Forms.Button();
+            this.Two = new System.Windows.Forms.Button();
+            this.Three = new System.Windows.Forms.Button();
+            this.Four = new System.Windows.Forms.Button();
+            this.Five = new System.Windows.Forms.Button();
+            this.Six = new System.Windows.Forms.Button();
+            this.Eight = new System.Windows.Forms.Button();
+            this.Seven = new System.Windows.Forms.Button();
+            this.Twelve = new System.Windows.Forms.Button();
+            this.Nine = new System.Windows.Forms.Button();
+            this.Eleven = new System.Windows.Forms.Button();
+            this.Ten = new System.Windows.Forms.Button();
+            this.Sixteen = new System.Windows.Forms.Button();
+            this.Fifteen = new System.Windows.Forms.Button();
+            this.Fourteen = new System.Windows.Forms.Button();
+            this.Thirteen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // One
+            // 
+            this.One.Location = new System.Drawing.Point(12, 81);
+            this.One.Name = "One";
+            this.One.Size = new System.Drawing.Size(97, 64);
+            this.One.TabIndex = 0;
+            this.One.Text = "button1";
+            this.One.UseVisualStyleBackColor = true;
+            this.One.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Two
+            // 
+            this.Two.Location = new System.Drawing.Point(115, 81);
+            this.Two.Name = "Two";
+            this.Two.Size = new System.Drawing.Size(97, 64);
+            this.Two.TabIndex = 1;
+            this.Two.Text = "button1";
+            this.Two.UseVisualStyleBackColor = true;
+            this.Two.Click += new System.EventHandler(this.Two_Click);
+            // 
+            // Three
+            // 
+            this.Three.Location = new System.Drawing.Point(218, 81);
+            this.Three.Name = "Three";
+            this.Three.Size = new System.Drawing.Size(97, 64);
+            this.Three.TabIndex = 2;
+            this.Three.Text = "button1";
+            this.Three.UseVisualStyleBackColor = true;
+            this.Three.Click += new System.EventHandler(this.Three_Click);
+            // 
+            // Four
+            // 
+            this.Four.Location = new System.Drawing.Point(321, 81);
+            this.Four.Name = "Four";
+            this.Four.Size = new System.Drawing.Size(97, 64);
+            this.Four.TabIndex = 3;
+            this.Four.Text = "button1";
+            this.Four.UseVisualStyleBackColor = true;
+            this.Four.Click += new System.EventHandler(this.Four_Click);
+            // 
+            // Five
+            // 
+            this.Five.Location = new System.Drawing.Point(12, 151);
+            this.Five.Name = "Five";
+            this.Five.Size = new System.Drawing.Size(97, 64);
+            this.Five.TabIndex = 4;
+            this.Five.Text = "button1";
+            this.Five.UseVisualStyleBackColor = true;
+            this.Five.Click += new System.EventHandler(this.Five_Click);
+            // 
+            // Six
+            // 
+            this.Six.Location = new System.Drawing.Point(115, 151);
+            this.Six.Name = "Six";
+            this.Six.Size = new System.Drawing.Size(97, 64);
+            this.Six.TabIndex = 5;
+            this.Six.Text = "button1";
+            this.Six.UseVisualStyleBackColor = true;
+            this.Six.Click += new System.EventHandler(this.Six_Click);
+            // 
+            // Eight
+            // 
+            this.Eight.Location = new System.Drawing.Point(321, 151);
+            this.Eight.Name = "Eight";
+            this.Eight.Size = new System.Drawing.Size(97, 64);
+            this.Eight.TabIndex = 6;
+            this.Eight.Text = "button1";
+            this.Eight.UseVisualStyleBackColor = true;
+            this.Eight.Click += new System.EventHandler(this.Eight_Click);
+            // 
+            // Seven
+            // 
+            this.Seven.Location = new System.Drawing.Point(218, 151);
+            this.Seven.Name = "Seven";
+            this.Seven.Size = new System.Drawing.Size(97, 64);
+            this.Seven.TabIndex = 7;
+            this.Seven.Text = "button1";
+            this.Seven.UseVisualStyleBackColor = true;
+            this.Seven.Click += new System.EventHandler(this.Seven_Click);
+            // 
+            // Twelve
+            // 
+            this.Twelve.Location = new System.Drawing.Point(321, 221);
+            this.Twelve.Name = "Twelve";
+            this.Twelve.Size = new System.Drawing.Size(97, 64);
+            this.Twelve.TabIndex = 8;
+            this.Twelve.Text = "button1";
+            this.Twelve.UseVisualStyleBackColor = true;
+            this.Twelve.Click += new System.EventHandler(this.Twelve_Click);
+            // 
+            // Nine
+            // 
+            this.Nine.Location = new System.Drawing.Point(12, 221);
+            this.Nine.Name = "Nine";
+            this.Nine.Size = new System.Drawing.Size(97, 64);
+            this.Nine.TabIndex = 9;
+            this.Nine.Text = "button1";
+            this.Nine.UseVisualStyleBackColor = true;
+            this.Nine.Click += new System.EventHandler(this.Nine_Click);
+            // 
+            // Eleven
+            // 
+            this.Eleven.Location = new System.Drawing.Point(218, 221);
+            this.Eleven.Name = "Eleven";
+            this.Eleven.Size = new System.Drawing.Size(97, 64);
+            this.Eleven.TabIndex = 10;
+            this.Eleven.Text = "button1";
+            this.Eleven.UseVisualStyleBackColor = true;
+            this.Eleven.Click += new System.EventHandler(this.Eleven_Click);
+            // 
+            // Ten
+            // 
+            this.Ten.Location = new System.Drawing.Point(115, 221);
+            this.Ten.Name = "Ten";
+            this.Ten.Size = new System.Drawing.Size(97, 64);
+            this.Ten.TabIndex = 11;
+            this.Ten.Text = "button1";
+            this.Ten.UseVisualStyleBackColor = true;
+            this.Ten.Click += new System.EventHandler(this.Ten_Click);
+            // 
+            // Sixteen
+            // 
+            this.Sixteen.Location = new System.Drawing.Point(321, 291);
+            this.Sixteen.Name = "Sixteen";
+            this.Sixteen.Size = new System.Drawing.Size(97, 64);
+            this.Sixteen.TabIndex = 12;
+            this.Sixteen.Text = "button1";
+            this.Sixteen.UseVisualStyleBackColor = true;
+            this.Sixteen.Click += new System.EventHandler(this.Sixteen_Click);
+            // 
+            // Fifteen
+            // 
+            this.Fifteen.Location = new System.Drawing.Point(218, 291);
+            this.Fifteen.Name = "Fifteen";
+            this.Fifteen.Size = new System.Drawing.Size(97, 64);
+            this.Fifteen.TabIndex = 13;
+            this.Fifteen.Text = "button1";
+            this.Fifteen.UseVisualStyleBackColor = true;
+            this.Fifteen.Click += new System.EventHandler(this.Fifteen_Click);
+            // 
+            // Fourteen
+            // 
+            this.Fourteen.Location = new System.Drawing.Point(115, 291);
+            this.Fourteen.Name = "Fourteen";
+            this.Fourteen.Size = new System.Drawing.Size(97, 64);
+            this.Fourteen.TabIndex = 14;
+            this.Fourteen.Text = "button1";
+            this.Fourteen.UseVisualStyleBackColor = true;
+            this.Fourteen.Click += new System.EventHandler(this.Fourteen_Click);
+            // 
+            // Thirteen
+            // 
+            this.Thirteen.Location = new System.Drawing.Point(12, 291);
+            this.Thirteen.Name = "Thirteen";
+            this.Thirteen.Size = new System.Drawing.Size(97, 64);
+            this.Thirteen.TabIndex = 15;
+            this.Thirteen.Text = "button1";
+            this.Thirteen.UseVisualStyleBackColor = true;
+            this.Thirteen.Click += new System.EventHandler(this.Thirteen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Thirteen);
+            this.Controls.Add(this.Fourteen);
+            this.Controls.Add(this.Fifteen);
+            this.Controls.Add(this.Sixteen);
+            this.Controls.Add(this.Ten);
+            this.Controls.Add(this.Eleven);
+            this.Controls.Add(this.Nine);
+            this.Controls.Add(this.Twelve);
+            this.Controls.Add(this.Seven);
+            this.Controls.Add(this.Eight);
+            this.Controls.Add(this.Six);
+            this.Controls.Add(this.Five);
+            this.Controls.Add(this.Four);
+            this.Controls.Add(this.Three);
+            this.Controls.Add(this.Two);
+            this.Controls.Add(this.One);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button One;
+        private System.Windows.Forms.Button Two;
+        private System.Windows.Forms.Button Three;
+        private System.Windows.Forms.Button Four;
+        private System.Windows.Forms.Button Five;
+        private System.Windows.Forms.Button Six;
+        private System.Windows.Forms.Button Eight;
+        private System.Windows.Forms.Button Seven;
+        private System.Windows.Forms.Button Twelve;
+        private System.Windows.Forms.Button Nine;
+        private System.Windows.Forms.Button Eleven;
+        private System.Windows.Forms.Button Ten;
+        private System.Windows.Forms.Button Sixteen;
+        private System.Windows.Forms.Button Fifteen;
+        private System.Windows.Forms.Button Fourteen;
+        private System.Windows.Forms.Button Thirteen;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string input = string.Empty; //string storing user input
+        string operand1 = string.Empty; //string storing first operand
+        string operand2 = string.Empty; //string storing second operand 
+        char operation;  //char for operation
+        double result = 0.0; //calculated result
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Two_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Three_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Four_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Five_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Six_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Seven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Eight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ten_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Eleven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Twelve_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Thirteen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Fourteen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Fifteen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sixteen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
